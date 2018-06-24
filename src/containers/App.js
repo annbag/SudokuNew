692df735356d8d206b19571ac0c121e22259cf07
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-24 23:36:09 +0200

    add state in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-//szkic
-render() {
-    return (
-		<div className="App">
-		   <h1>Sudoku</h1>
-		   <Board />
-		   <div className="buttons">
-		       <button>Check</button>
-		       <button>New Game</button>
-		       <button>Solve</button>
-		       <button>Restart</button>
-		   </div>
-		</div>
-	)
+class App extends React.Component {
+	constuctor(props){
+		super(props);
+		this.state = {
+			initialBoard: '',
+			board: ''
+		}
+	}
+	//szkic
+	render() {
+	    return (
+			<div className="App">
+			   <h1>Sudoku</h1>
+			   <Board />
+			   <div className="buttons">
+			       <button>Check</button>
+			       <button>New Game</button>
+			       <button>Solve</button>
+			       <button>Restart</button>
+			   </div>
+			</div>
+		)
+	}
 }
+
