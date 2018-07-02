ad8105afb9e9e531858d7245cc325b185ff19fd2
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-02 20:14:21 +0200

    change2 in Tile.js

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Tile = props => (
-	<input type="number" min="1" max="9" value={props.value} onChange=props.handleChange />
-);
+const Tile = props => {
+	return (
+		<input type="number" min="1" max="9" value={props.value} onChange=props.handleChange />
+	);
+};
 
 export default Tile;
-
-
