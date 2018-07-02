22dd10d928b41dc1c8dbeac4bd91890c0c4911c3
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-02 20:33:33 +0200

    add style for board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Tile from  './Tile.js';
+import './Board.css'; 
 
 const Board = props => {
 	return(
-		<div>
+		<div className="board" >
 			{props.board.split("").map((item, index) => <Tile value={item} 
 			handleChange={(e) => props.handleChange(index, e.target.value)}/> )}
 		</div>
